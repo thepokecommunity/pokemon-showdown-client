fb9d49c7f58435e4a05e4e864e407242a3304de5
Dakota <dakotasankey@gmail.com>
2021-03-28 18:44:06 -0700

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -18,9 +18,9 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'showdown',
-	host: 'sim3.psim.us',
-	port: 443,
+	id: 'pokecommunity',
+	host: 'battle.pokecommunity.com',
+	port: 8000,
 	httpport: 8000,
 	altport: 80,
 	registered: true
